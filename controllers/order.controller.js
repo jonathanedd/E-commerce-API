84db35edc4ee9c9bb2b7c59fb729c8f330984c1f
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2022-05-31 00:03:06 -0500

    Added quantity validation before to exceed

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -10,92 +10,102 @@ const { status } = require("express/lib/response");
 const addUserCart = catchAsync(async (req, res, next) => {});
 
 const addProductToCart = catchAsync(async (req, res, next) => {
-  // const { productId, quantity } = req.body;
-  // const { sessionUser } = req;
-
-  // const userHasCart = await Cart.findOne({
-  //   where: { userId: sessionUser.id, status: "active" },
-  //   include: [
-  //     {
-  //       model: ProductsInCart,
-  //       where: { status: "active" },
-  //       attributes: { include: ["cartId"] },
-  //     },
-  //   ],
-  // });
-
-  // if (!userHasCart) {
-  //   const createCart = await Cart.create({ userId: sessionUser.id });
-
-  //   res.status(200).json({
-  //     status: "Added",
-  //     createCart,
-  //   });
-  // } else {
-  //   const productInCart = await ProductsInCart.create({
-  //     cartId: userHasCart.id,
-  //     productId,
-  //     quantity,
-  //   });
-
-  //   const findProduct = await ProductsInCart.findAll({
-  //     where: { productId },
-  //     include: [
-  //       {
-  //         model: Product,
-  //         attributes: {
-  //           exclude: ["categoryId", "createdAt", "updatedAt"],
-  //         },
-  //       },
-  //     ],
-  //   });
-
-  //   res.status(200).json({
-  //     status: "Product added to cart",
-  //     productInCart,
-  //     findProduct,
-  //   });
-  // }
-
   const { productId, quantity } = req.body;
-
   const { sessionUser } = req;
 
-  const userCart = await Cart.findOne({
+  const userHasCart = await Cart.findOne({
     where: { userId: sessionUser.id, status: "active" },
     include: [
       {
         model: ProductsInCart,
         where: { status: "active" },
-        include: [
-          {
-            model: Product,
-          },
-        ],
+        attributes: { include: ["cartId"] },
       },
     ],
   });
 
-  // Validation for cart
-  if (!userCart) {
-    const assignCart = await Cart.create({ userId: sessionUser.id });
+  if (!userHasCart) {
+    const createCart = await Cart.create({ userId: sessionUser.id });
 
     res.status(200).json({
-      status: "Cart added to user",
-      assignCart,
+      status: "Added",
+      createCart,
     });
   } else {
-    const addProduct = await ProductsInCart.create({
-      cartId: userCart.id,
+    const productInCart = await ProductsInCart.create({
+      cartId: userHasCart.id,
       productId,
       quantity,
     });
 
+    const findProduct = await ProductsInCart.findAll({
+      where: { productId },
+      include: [
+        {
+          model: Product,
+          attributes: {
+            exclude: ["categoryId", "createdAt", "updatedAt"],
+          },
+        },
+      ],
+    });
+
+    const productExist = await Product.findOne({
+      where: { status: "active" },
+    });
+
+    if (productInCart.quantity > productExist.quantity) {
+      next(new AppError("Product is Sold Out", 400));
+    }
+
     res.status(200).json({
-      status: "success",
-      addProduct,
+      status: "Product added to cart",
+      productInCart,
+      findProduct,
     });
   }
+
+  // --------------------------------OPTION 2 ------------------------------------------
+
+  // const { productId, quantity } = req.body;
+
+  // const { sessionUser } = req;
+
+  // const userCart = await Cart.findOne({
+  //   where: { userId: sessionUser.id, status: "active" },
+  //   include: [
+  //     {
+  //       model: ProductsInCart,
+  //       where: { status: "active" },
+  //       include: [
+  //         {
+  //           model: Product,
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // });
+
+  // // Validation for cart
+  // if (!userCart) {
+  //   const assignCart = await Cart.create({ userId: sessionUser.id });
+
+  //   res.status(200).json({
+  //     status: "Cart added to user",
+  //     assignCart,
+  //   });
+  // } else {
+  //   const addProduct = await ProductsInCart.create({
+  //     cartId: userCart.id,
+  //     productId,
+  //     quantity,
+  //   });
+
+  //   res.status(200).json({
+  //     status: "success",
+  //     addProduct,
+  //   });
+  // }
 });
 
 const updateProductToCart = catchAsync(async (req, res, next) => {});
