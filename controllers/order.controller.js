a8a08c95fbe6519d09876098ffad5b7306b03f2b
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2022-05-30 23:37:09 -0500

    Working in orders enpoints

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -5,53 +5,95 @@ const { Product } = require("../models/product.model");
 
 const { catchAsync } = require("../utils/catchAsync");
 const { AppError } = require("../utils/appError");
+const { status } = require("express/lib/response");
 
 const addUserCart = catchAsync(async (req, res, next) => {});
 
 const addProductToCart = catchAsync(async (req, res, next) => {
+  // const { productId, quantity } = req.body;
+  // const { sessionUser } = req;
+
+  // const userHasCart = await Cart.findOne({
+  //   where: { userId: sessionUser.id, status: "active" },
+  //   include: [
+  //     {
+  //       model: ProductsInCart,
+  //       where: { status: "active" },
+  //       attributes: { include: ["cartId"] },
+  //     },
+  //   ],
+  // });
+
+  // if (!userHasCart) {
+  //   const createCart = await Cart.create({ userId: sessionUser.id });
+
+  //   res.status(200).json({
+  //     status: "Added",
+  //     createCart,
+  //   });
+  // } else {
+  //   const productInCart = await ProductsInCart.create({
+  //     cartId: userHasCart.id,
+  //     productId,
+  //     quantity,
+  //   });
+
+  //   const findProduct = await ProductsInCart.findAll({
+  //     where: { productId },
+  //     include: [
+  //       {
+  //         model: Product,
+  //         attributes: {
+  //           exclude: ["categoryId", "createdAt", "updatedAt"],
+  //         },
+  //       },
+  //     ],
+  //   });
+
+  //   res.status(200).json({
+  //     status: "Product added to cart",
+  //     productInCart,
+  //     findProduct,
+  //   });
+  // }
+
   const { productId, quantity } = req.body;
+
   const { sessionUser } = req;
 
-  const userHasCart = await Cart.findOne({
+  const userCart = await Cart.findOne({
     where: { userId: sessionUser.id, status: "active" },
     include: [
       {
-        model: User,
-        attributes: { exclude: ["password"] },
+        model: ProductsInCart,
+        where: { status: "active" },
+        include: [
+          {
+            model: Product,
+          },
+        ],
       },
     ],
   });
 
-  if (!userHasCart) {
-    const createCart = await Cart.create({ userId: sessionUser.id });
+  // Validation for cart
+  if (!userCart) {
+    const assignCart = await Cart.create({ userId: sessionUser.id });
 
     res.status(200).json({
-      status: "Added",
-      findProduct,
-      createCart,
+      status: "Cart added to user",
+      assignCart,
     });
   } else {
     const addProduct = await ProductsInCart.create({
-      cartId: userHasCart.id,
+      cartId: userCart.id,
       productId,
       quantity,
     });
 
-    const findProduct = await ProductsInCart.findAll({
-      where: { status: "active" },
-      include: [
-        {
-          model: Product,
-          attributes: {
-            exclude: ["categoryId", "userId", "createdAt", "updatedAt"],
-          },
-        },
-      ],
-    });
-
     res.status(200).json({
-      status: "Product added to cart",
-      findProduct,
+      status: "success",
+      addProduct,
     });
   }
 });
